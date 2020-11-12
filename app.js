ff4df43e4a2f4c7b316b67854516dfbc291bd8da
Fahru Rozi <m.fahruu.rozii@gmail.com>
2020-11-12 22:46:39 +0700

    add sample validation

--- app.js
+++ app.js
@@ -8,8 +8,15 @@ mongoose.connect('mongodb://localhost:27017/fruitsDB', {
 });
 
 const fruitsSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+    name: {
+        type: String,
+        required: [true, "Please check your data entry, no name specified!"]
+    },
+    rating: {
+        type : Number,
+        min :1,
+        max : 10
+    },
     review: String
 });
 
